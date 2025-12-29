744695838b6a380d22ae7e6f97d65a806c01a7e3
YaIsrak <israkdreamstar@gmail.com>
2025-12-30 00:48:40 +0600

    cart model, page and functionality

---
+++ carts/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('', views.cart_view, name='cart'),
+    path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove/<int:product_id>/', views.remove_cart, name='remove_cart'),
+    path('remove_item/<int:product_id>/', views.remove_cart_item, name='remove_cart_item'),
+]
