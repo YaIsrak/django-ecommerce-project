ccef0520dc0fe58eeb4a96210c58fae12bd8caba
YaIsrak <israkdreamstar@gmail.com>
2025-12-29 23:55:40 +0600

    Product  Page

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.store, name='store_home'),
     path('<slug:category_slug>/', views.store, name='products_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
 ]
