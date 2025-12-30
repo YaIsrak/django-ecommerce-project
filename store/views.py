0b9334003eab9b9c70c3b47bee64900386a1cec2
YaIsrak <israkdreamstar@gmail.com>
2025-12-30 20:38:38 +0600

    Product cart show

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from store.models import Product
 from category.models import Category
 from django.shortcuts import get_object_or_404
+from carts.models import CartItem
+from carts.views import _cart_id
+from django.http import HttpResponse
+
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -15,7 +19,6 @@ def store(request, category_slug=None):
     else:
         products = Product.objects.all().filter(is_available=True)
         product_count = products.count()
-        # categories = Category.objects.all()
 
     context = {
         'products': products,
@@ -27,10 +30,15 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+
+        # return HttpResponse(in_cart)
+        # exit()
     except Exception as e:
         raise e
 
     context = {
         'single_product': single_product,
+        "in_cart": in_cart,
     }
     return render(request, 'store/product_detail.html', context)
