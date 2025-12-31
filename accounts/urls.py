7434aedc1438cf1eef475d706beacabdb67e6058
YaIsrak <israkdreamstar@gmail.com>
2025-12-31 19:36:25 +0600

    Dashboard done

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('sign-up/', views.signup, name='sign_up'),
     path('sign-in/', views.signin, name='sign_in'),
     path('sign-out/', views.signout, name='sign_out'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+    path('forgot-password/', views.forgot_password, name='forgot_password'),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,12 @@ from django.contrib import messages
 from django.shortcuts import redirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 
 # Create your views here.
 def signup(request):
@@ -52,3 +58,34 @@ def signout(request):
     auth.logout(request)
     messages.success(request, 'You are logged out.')
     return redirect('sign_in')
+
+
+@login_required(login_url='sign_in')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+def forgot_password(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact=email)
+
+            current_site = get_current_site(request)
+            mail_subject = 'Reset Your Password'
+            message = render_to_string('accounts/reset-password-email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
+            messages.success(request, 'Password reset email has been sent to your email address.')
+            return redirect('sign_in')
+        else:
+            messages.error(request, 'Account does not exist!')
+            return redirect('forgot_password')
+    return render(request, 'accounts/forgot-password.html')
